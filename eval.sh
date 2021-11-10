0064745ea3be310b9edc253ea7a04be772cc72d1
叶宇轩 <yeyuxuan@pc-136-83.customer.ask4.lan>
2021-11-10 12:13:55 +0000

    retry best

--- eval.sh
+++ eval.sh
@@ -2,4 +2,4 @@ export DATASET=~/Dataset
 export RESULT=./result
 export TOKENIZERS_PARALLELISM=true
 
-python -u experiments/evaluate.py --dataset $DATASET/t1 --method clust --output $RESULT/t17.clust.json > log.txt
+python -u experiments/evaluate.py --dataset $DATASET/t1 --method clust --output /content/drive/MyDrive/results/news-tls-best.json
