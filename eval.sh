e744558c9d9d975665cf3b0032890fdc54586fd7
叶宇轩 <yeyuxuan@pc-136-83.customer.ask4.lan>
2021-11-10 15:54:11 +0000

    retry large

--- eval.sh
+++ eval.sh
@@ -2,4 +2,4 @@ export DATASET=./dataset
 export RESULT=./result
 export TOKENIZERS_PARALLELISM=true
 
-python -u experiments/evaluate.py --dataset $DATASET/Timeline17 --method clust --output /content/drive/MyDrive/results/news-tls-best.json
+python -u experiments/evaluate.py --dataset $DATASET/Timeline17 --method clust --output /content/drive/MyDrive/results/news-tls-best-large.json
