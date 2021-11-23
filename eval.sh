d8bd183a8e7faad2d389c85a3017dc03c4a4976a
叶宇轩 <yeyuxuan@Yuxuans-MacBook-Pro.local>
2021-11-24 01:31:35 +0800

    markov+pegasus clip8

--- eval.sh
+++ eval.sh
@@ -2,4 +2,4 @@ export DATASET=./dataset
 export RESULT=./result
 export TOKENIZERS_PARALLELISM=true
 
-python -u experiments/evaluate.py --dataset $DATASET/Timeline17 --method clust --output /content/drive/MyDrive/results/markov-pegasus-clip7.json
+python -u experiments/evaluate.py --dataset $DATASET/Timeline17 --method clust --output /content/drive/MyDrive/results/markov-pegasus-clip8.json
