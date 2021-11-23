12b8b28f46821af59afe5153b3ace5ffc02cc0ff
叶宇轩 <yeyuxuan@Yuxuans-MacBook-Pro.local>
2021-11-23 20:21:26 +0800

    markov+pegasus clip4

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -189,7 +189,7 @@ def main(args):
             cluster_ranker=cluster_ranker,
             clusterer=clusterer,
             summarizer=summarizer,
-            clip_sents=17,
+            clip_sents=4,
             unique_dates=True,
         )
     else:
