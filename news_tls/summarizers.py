8c4feeeaa0bd31f18e14c9c0ce23d53a65161d22
叶宇轩 <yeyuxuan@Yuxuans-MacBook-Pro.local>
2021-11-26 12:01:48 +0800

    ap+pegasus-human+clip6

--- news_tls/summarizers.py
+++ news_tls/summarizers.py
@@ -15,7 +15,8 @@ class Summarizer:
 
 class Pegasus():
     def __init__(self):
-        self.model_name = 'google/pegasus-large'
+        #self.model_name = 'google/pegasus-large'
+        self.model_name = 'human-centered-summarization/financial-summarization-pegasus'
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.tokenizer = PegasusTokenizer.from_pretrained(self.model_name)
         self.model = PegasusForConditionalGeneration.from_pretrained(self.model_name).to(self.device)
