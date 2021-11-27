41bf08d526c160f30926d46bacdf71b7df31242a
叶宇轩 <yeyuxuan@Yuxuans-MacBook-Pro.local>
2021-11-28 03:59:35 +0800

    crisis-clust-clip4

--- news_tls/data.py
+++ news_tls/data.py
@@ -354,8 +354,10 @@ class Timeline:
         self.time_to_summaries = dict((t, s) for t, s in items)
         self.date_to_summaries = dict((t.date(), s) for t, s in items)
         self.times = sorted(self.time_to_summaries)
+        print(self.time_to_summaries)
 
     def __getitem__(self, item):
+        print(f'item={item}')
         return self.time_to_summaries[item]
 
     def __len__(self):
