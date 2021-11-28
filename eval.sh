776a6baaf4a72b5f3a70235ed176f5a882703f64
叶宇轩 <yeyuxuan@Yuxuans-MacBook-Pro.local>
2021-11-28 10:17:52 +0800

    crisis-clust-clip6

--- eval.sh
+++ eval.sh
@@ -1,4 +1,4 @@
 export DATASET=/content/drive/MyDrive/dataset/crisis
 export RESULT=./result
 
-python -u experiments/evaluate.py --dataset $DATASET --method clust --output /content/drive/MyDrive/results/crisis/clust-clip5.json
+python -u experiments/evaluate.py --dataset $DATASET --method clust --output /content/drive/MyDrive/results/crisis/clust-clip6.json
