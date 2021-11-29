60e891c3077c01e433b30743dc6d40876ad7c356
叶宇轩 <yeyuxuan@Yuxuans-MacBook-Pro.local>
2021-11-29 20:34:15 +0800

    crisis-amarkov-pegasus-clipall

--- eval.sh
+++ eval.sh
@@ -1,4 +1,4 @@
 export DATASET=/content/drive/MyDrive/dataset/crisis
 export RESULT=./result
 
-python -u experiments/evaluate.py --dataset $DATASET --method clust --output /content/drive/MyDrive/results/crisis/markov-pegasus-clip10.json
+python -u experiments/evaluate.py --dataset $DATASET --method clust --output /content/drive/MyDrive/results/crisis/markov-pegasus-clipall.json
