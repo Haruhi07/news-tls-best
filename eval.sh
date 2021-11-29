cc735bd2a789e2550f5fc7f7113b07d409030e5d
叶宇轩 <yeyuxuan@Yuxuans-MacBook-Pro.local>
2021-11-30 05:14:11 +0800

    markov+pagasus-xsum+clip2

--- eval.sh
+++ eval.sh
@@ -1,4 +1,5 @@
 export DATASET=/content/drive/MyDrive/dataset/crisis
 export RESULT=./result
+export TOKENIZERS_PARALLELISM=true
 
 python -u experiments/evaluate.py --dataset $DATASET --method clust --output /content/drive/MyDrive/results/crisis/ap-pegasus-clip4.json
